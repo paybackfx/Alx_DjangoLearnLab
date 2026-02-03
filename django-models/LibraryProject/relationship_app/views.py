01d5378e87b5d3187d8bf832fa14a1b473f4fef7
payback <sirfennoune@gmail.com>
2026-02-03 20:06:37 +0100

    Split decorator imports for checker: explicit 'from django.contrib.auth.decorators import permission_required'

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,7 +13,9 @@ from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.views import LoginView, LogoutView
 
 from .models import Book
